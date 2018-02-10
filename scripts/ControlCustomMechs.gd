77510e4d1e1f306861f27f549f40364e3c1bd25b
Lightnet <darkneter@gmail.com>
2018-02-09 22:13:46 -0800

    update the ui and scripts.

--- scripts/ControlCustomMechs.gd
+++ scripts/ControlCustomMechs.gd
@@ -1,11 +1,27 @@
 extends Control
 
 var nodemech
+var btnequips
+var equipgroups
 
 func _ready():
 	nodemech =get_node("../../Spatial/MeshInstance")
-	print(nodemech)
+	#print(nodemech)
+	btnequips = get_node("MBEquips")
+	equipgroups = btnequips.get_popup()
+	equipgroups.add_item("Head")
+	equipgroups.add_item("Body")
+	equipgroups.add_item("Arms")
+	equipgroups.add_item("Legs")
+	equipgroups.add_item("Parts")
+	
+	equipgroups.connect("id_pressed", self, "_on_item_pressed")
 	pass
+	
+func _on_item_pressed(ID):
+	var pop  = btnequips.get_popup()
+	btnequips.set_text(equipgroups.get_item_text(ID))
+
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
@@ -27,9 +43,6 @@ func _on_HSlider_value_changed( value ):
 		#print(String(value))
 		var rot = Quat(Vector3(0, 1, 0), value * -1)
 		var t = nodemech.get_transform()
-		
 		nodemech.set_transform(Transform(rot, t.origin))
-		
-		
 		pass
 	pass
