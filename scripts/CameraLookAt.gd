b5ab2b8beacad4000c76b04ec608b1342f56ccc2
Lightnet <darkneter@gmail.com>
2018-02-22 06:42:14 -0800

    Menu added builds.

--- scripts/CameraLookAt.gd
+++ scripts/CameraLookAt.gd
@@ -1,12 +1,11 @@
 extends Node
-
-var camera
-var obj
-var target_dir = Vector3(0,0,0)
-
-export (NodePath) var objpath
-var objnode
-var sphere
+#var camera
+#var obj
+#var target_dir = Vector3(0,0,0)
+#var angle = 0
+#export (NodePath) var objpath
+#var objnode
+#var sphere
 func _ready():
 	#camera = get_node("/root/Node/Spatial/Camera")
 	#obj = get_node("MeshInstance")
@@ -16,11 +15,12 @@ func _ready():
 	set_process(true)
 	pass
 	
-var angle = 0
+
 
 func _process(delta):
-	# Called every frame. Delta is time since last frame.
-	# Update game logic here.
+	var target = get_viewport().get_camera().global_transform.origin
+	look_at_from_position(self.global_transform.origin,target,Vector3(0,1,0))
+	
 	#get_global_transform
 	
 	#var target = objnode.global_transform.origin
@@ -43,12 +43,4 @@ func _process(delta):
 	#look_at(target_dir,Vector3(0,1,0))
 	#looking_at(target_dir,Vector3(0,1,0))
 	#look_at_from_position(self.global_transform.origin,objnode.global_transform.origin,Vector3(0,1,0))
-	
-	var target = get_viewport().get_camera().global_transform.origin
-	look_at_from_position(self.global_transform.origin,target,Vector3(0,1,0))
-	
-	
-	
-	
-	
 	pass
